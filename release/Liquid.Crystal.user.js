61e338a4766aefdee88cdd1de1985f20031c2147
xZorro <108305740+UUID1606@users.noreply.github.com>
2023-02-02 23:36:32 +0000

    Add files via upload

---
+++ release/Liquid.Crystal.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Liquid Crystal
+// @version      1.00
+// @description  Free open-source game cheat for Tanki Online.
+// @author       Zorro
+// @match        https://*.test-eu.tankionline.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=test.tankionline.com
+
+// @require      https://github.com/js-cookie/js-cookie/releases/download/v3.0.1/js.cookie.min.js
+// @require      https://raw.githubusercontent.com/UUID1606/Liquid-Crystal-V1/main/Files/jquery.min.js
+// @require      https://raw.githubusercontent.com/UUID1606/Liquid-Crystal-V1/main/Files/imgui.umd.js
+// @require      https://raw.githubusercontent.com/UUID1606/Liquid-Crystal-V1/main/Files/imgui_impl.umd.js
+// @require      https://raw.githubusercontent.com/UUID1606/Liquid-Crystal-V1/main/Files/isKeyPressed.min.js
+
+// @require https://raw.githubusercontent.com/UUID1606/Liquid-Crystal-V1/main/Files/Liquid.Crystal.js
+// @downloadURL https://raw.githubusercontent.com/UUID1606/Liquid-Crystal-V1/main/Files/Liquid.Crystal.js
+// @updateURL https://raw.githubusercontent.com/UUID1606/Liquid-Crystal-V1/main/Files/Liquid.Crystal.js
+
+// ==/UserScript==
+
+//By using this cheat you are admiting that xZorro is the best player in the world
+//Используя этот чит, вы признаете, что xZorro — лучший игрок в мире
+
